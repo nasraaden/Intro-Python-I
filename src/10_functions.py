ad78a6d6e8c44197bdcecb4afc2c98241b405dc9
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-04-07 18:32:34 -0700

    finished functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,20 @@
 
 # YOUR CODE HERE
 
+
+def is_even(num):
+    if num % 2 == 0:
+        print('Even!')
+    else:
+        print('Odd!')
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
 
+
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+is_even(num)
